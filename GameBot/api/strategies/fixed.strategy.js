c1bc807fce5dea4af1cf24a463f3fd95faadbf62
Nico Meisenzahl <nico@meisenzahl.org>
2020-12-09 13:01:10 +0100

    adds tuning

--- GameBot/api/strategies/fixed.strategy.js
+++ GameBot/api/strategies/fixed.strategy.js
@@ -8,10 +8,20 @@ class FixedStrategy {
     }
 
     pick() {
+
+        var bet = null;
+        if (process.env.FF_BETS) {
+            // The bet of Humans that they will win
+            // value of 1 means if Humans win, they will get 2x of the score - if they loose they loose 2x of the score
+            // If null the Humans player does not support bets, value needs to be between 0 and 1
+            bet = Math.random();
+        }
+        
         return {
             "playerType": "node",
             "player": os.hostname(),
             "text": this.choice,
+            "bet": bet,
             "value": RPSLSChoices.indexOf(this.choice)
         };
     }
